3f2475e4bf0d46a529ffc78d3556a3e22afdb9b0
Mutiur03 <mutiur5bb@gmail.com>
2025-08-09 18:45:37 +0600

    image fixed

--- next.config.ts
+++ next.config.ts
@@ -8,13 +8,12 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  
+
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "",
-        port: "",
+        hostname: "**",
         pathname: "/**",
       },
     ],
